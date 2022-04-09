e52f6e09998223e3f4c6d60ab3f72cbab7b586d5
IamAyush <93813828+Ayushssss@users.noreply.github.com>
2022-04-09 15:33:45 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const {Collection, Client, Discord} = require('discord.js');
+const fs = require('fs')
+const config = require('./config.json');
+const client = new Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MEMBERS",
+        "GUILD_BANS",
+        "GUILD_INTEGRATIONS",
+        "GUILD_WEBHOOKS",
+        "GUILD_INVITES",
+        "GUILD_VOICE_STATES",
+        "GUILD_PRESENCES",
+        "GUILD_MESSAGES",
+        "GUILD_MESSAGE_REACTIONS",
+        "GUILD_MESSAGE_TYPING",
+        "DIRECT_MESSAGES",
+        "DIRECT_MESSAGE_REACTIONS",
+        "DIRECT_MESSAGE_TYPING",
+    ],
+});
+
+module.exports = client;
+
+client.commands = new Collection();
+client.aliases = new Collection();
+client.categories = fs.readdirSync("./commands/");
+
+["command", "event"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+});
+
+client.login(config.token)
